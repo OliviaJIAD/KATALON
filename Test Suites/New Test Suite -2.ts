470427fab0c9315f820c3ab01799ec9cac349e1f
u45656 <u45656@ITL0130.iad.lan>
2020-03-17 15:55:47 +0100

    site-web prod

---
+++ Test Suites/New Test Suite -2.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite -2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>426b7f12-624e-44f1-8bef-b740d1d964f5</testSuiteGuid>
+</TestSuiteEntity>
